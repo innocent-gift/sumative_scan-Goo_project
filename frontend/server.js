67e77021c1b2330aa886b136645ba17f0a08d636
innocent-gift <i.nkurunziz@alustudent.com>
2026-03-26 21:09:06 +0200

    feat: add frontend static server for Railway deployment

---
+++ frontend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Frontend running on port ${PORT}`));
